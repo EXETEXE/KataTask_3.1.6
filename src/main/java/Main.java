a8705882b4990018ad3c53e344657563b5f5161f
EXETEXE <exetexe1@email.com>
2022-12-20 17:04:15 +0300

    2022.12.20 Первый вариант решения

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "http://94.198.50.185:7081/api/users";
+
+        Util util = new Util();
+
+        User userToAdd = new User(3L, "James", "Brown", (byte) 33);
+
+        System.out.println(util.save(userToAdd));
+
+        System.out.println(util.getAll());
+
+        User userToPatch = new User(3L, "Thomas", "Shelby", (byte) 33);
+
+        System.out.println(util.patch(userToPatch));
+
+        System.out.println(util.getAll());
+
+        System.out.println(util.delete(3));
+
+        System.out.println(util.getAll());
+    }
+}
